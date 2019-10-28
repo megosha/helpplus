c7baf3a90ff1bff15c9d39afc464760bd3b55f0b
mvgosha <mvgosha@gmail.com>
2019-10-28 12:05:49 +0700

    developer mail changed

--- js/index.js
+++ js/index.js
@@ -151,73 +151,103 @@ $('#contact-center-form-2-form').validate({
 //------------------------------------------------------------------------------------
 //								CONTACT FORM SCRIPT
 //------------------------------------------------------------------------------------
+    $('form').submit(function(e){
+        e.preventDefault();
+        var name = $(".name", $(this)).val();
+        var tel = $(".tel", $(this)).val();
+        var form_data = {
+            'key':'SayalWLYeSxn',
+            'method':'send_email_client',
+            'message':'Имя: '+name+'\nТелефон: '+tel,
+        };
+        $.ajax({
+          type: "POST",
+          url: "http://utils.electis.ru/api/",
+          data: form_data,
+          success: function(){
+              if (name && tel) {
+                $('#contact-center-form-2-form').find('[type=submit]').button('complete');
+                //Use modal popups to display messages
+                $('#popup-center-countdown').modal('show');
+                $('.modal.in').modal( 'hide' );
+                }
+          },
+          error: function() {
+            $('#contact-center-form-2-form').find('[type=submit]').button('reset');
+          }
+        });
+    });
 
-$('#contact-center-form-2-form').submit(function () {
+
+// $('#contact-center-form-2-form').submit(function () {
     // submit the form
     //data area
-    var data = [];
-    var $fields = $(this).find('.form-group, div.radio');
-    $fields.each(function(indx, el){
-        if ($( el ).hasClass('radio')) {
-            var name = $( el ).find('.label-name').html();
-            var $radioinput = $(el).find('input');
-            $( el).find('input').each(function(indx, el){
-                if ( $(el)[0].checked) {
-                    var value = $(el).parent().find('span.lbl').html();
-                    data.push({ name: name, value: value, name_attr: $radioinput.attr('name') });
-                    return;
-                }
-            });
-        } else if ($( el ).find('input').attr('type') === 'checkbox') {
-            var $input = $( el ).find( 'input' );
-            data.push( {name: $input.attr( 'placeholder' ), value: $input[0].checked ? 'checked' : 'unchecked', name_attr: $input.attr('name')} );
-        } else if ($( el ).find('select')[0]) {
-            var name = $( el ).find('select option' ).val();
-            var $select = $(el).find('select');
-            data.push({ name: name, value: $select.val(), name_attr: $select.attr('name')});
-        } else if ($( el ).find('textarea')[0]) {
-            var $textarea = $(el).find('textarea');
-            data.push({ name: $textarea.attr('placeholder'), value: $textarea.val(), name_attr: $textarea.attr('name') });
-        } else {
-            var $input = $(el).find('input');
-            data.push({ name: $input.attr('placeholder'), value: $input.val(), name_attr: $input.attr('name') });
-        }
-    });
+
+    //
+    // var data = [];
+    // var $fields = $(this).find('.form-group, div.radio');
+    // $fields.each(function(indx, el){
+    //     if ($( el ).hasClass('radio')) {
+    //         var name = $( el ).find('.label-name').html();
+    //         var $radioinput = $(el).find('input');
+    //         $( el).find('input').each(function(indx, el){
+    //             if ( $(el)[0].checked) {
+    //                 var value = $(el).parent().find('span.lbl').html();
+    //                 data.push({ name: name, value: value, name_attr: $radioinput.attr('name') });
+    //                 return;
+    //             }
+    //         });
+    //     } else if ($( el ).find('input').attr('type') === 'checkbox') {
+    //         var $input = $( el ).find( 'input' );
+    //         data.push( {name: $input.attr( 'placeholder' ), value: $input[0].checked ? 'checked' : 'unchecked', name_attr: $input.attr('name')} );
+    //     } else if ($( el ).find('select')[0]) {
+    //         var name = $( el ).find('select option' ).val();
+    //         var $select = $(el).find('select');
+    //         data.push({ name: name, value: $select.val(), name_attr: $select.attr('name')});
+    //     } else if ($( el ).find('textarea')[0]) {
+    //         var $textarea = $(el).find('textarea');
+    //         data.push({ name: $textarea.attr('placeholder'), value: $textarea.val(), name_attr: $textarea.attr('name') });
+    //     } else {
+    //         var $input = $(el).find('input');
+    //         data.push({ name: $input.attr('placeholder'), value: $input.val(), name_attr: $input.attr('name') });
+    //     }
+    // });
     //end data area
-    if ($(this).valid()) {
-        $(this).find('[type=submit]').button('loading');
-        var form = new FormData();
-        var $inputFiles = $('.inputfile');
-        $inputFiles.each(function(indx, inputFile){
-            $.each(inputFile.files, function(i, file) {
-                form.append('file-' + indx + '-' + i, file);
-            });
-        });
-        form.append('data', JSON.stringify(data));
-        form.append('id', this.id);
-        var action = $(this).attr('action');
-        $.ajax({
-            url: action,
-            type: 'POST',
-            data: form,
-            cache: false,
-            contentType: false,
-            processData: false,
-            success: function () {
-		$('#contact-center-form-2-form').find('[type=submit]').button('complete');
-		//Use modal popups to display messages
-		$('#popup-center-countdown').modal('show');
-		$('.modal.in').modal( 'hide' );
-	},
-            error: function () {
-		$('#contact-center-form-2-form').find('[type=submit]').button('reset');
-	}
-        });
-    } else {
-        //if data was invalidated
-    }
-    return false;
-});
+//     if ($(this).valid()) {
+//         $(this).find('[type=submit]').button('loading');
+//         var form = new FormData();
+//         var $inputFiles = $('.inputfile');
+//         $inputFiles.each(function(indx, inputFile){
+//             $.each(inputFile.files, function(i, file) {
+//                 form.append('file-' + indx + '-' + i, file);
+//             });
+//         });
+//         form.append('data', JSON.stringify(data));
+//         form.append('id', this.id);
+//         var action = $(this).attr('action');
+//         $.ajax({
+//             url: 'http://utils.electis.ru/api/?key=SayalWLYeSxn&method=send_email_client&subject=subj&message=mes',
+//             type: 'POST',
+//             data: form,
+//             cache: false,
+//             contentType: false,
+//             processData: false,
+//             success: function () {
+//                 alert("qweqwe");
+// 		$('#contact-center-form-2-form').find('[type=submit]').button('complete');
+// 		//Use modal popups to display messages
+// 		$('#popup-center-countdown').modal('show');
+// 		$('.modal.in').modal( 'hide' );
+// 	},
+//             error: function () {
+// 		$('#contact-center-form-2-form').find('[type=submit]').button('reset');
+// 	}
+//         });
+//     } else {
+//         //if data was invalidated
+//     }
+//     return false;
+// });
 //start spr-countdown
 var $popup_center_countdown_countdown = $('#popup-center-countdown-countdown');
 $popup_center_countdown_countdown.countdown('2019/11/18 23:59:59', function (event) {
